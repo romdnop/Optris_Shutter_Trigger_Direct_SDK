91e98a212102b25ccc76b717fb3918416a2dca03
Dmitry Romanov <dimaromanov2010@gmail.com>
2025-03-19 14:27:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+
+A small program that saves IR images from Xi410 camera by Optris.
+
+# Useful links
+
+https://sdk.optris.com/libirimager2/html/Overview.html
---
+++ sdk/version.h
@@ -0,0 +1,6 @@
+#include <string>
+
+static std::string libirimager_version_string = "8.8.5";
+static std::string libirimager_commit_string  = "76bc61b8"; 
+static std::string libirimager_commit_branch  = "Release_8.9.0"; 
+static std::string libirimager_commit_date    = "Wed Sep 6 15:14:11 2023";
