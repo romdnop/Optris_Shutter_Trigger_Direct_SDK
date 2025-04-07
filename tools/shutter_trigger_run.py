0f144770751528f83a2a5e11ea77e8b0168fd11e
Dmitry Romanov <dimaromanov2010@gmail.com>
2025-04-07 14:11:54 +0200

    Added python script for batch execution

--- tools/shutter_trigger_run.py
+++ tools/shutter_trigger_run.py
@@ -4,10 +4,20 @@ import argparse
 
 cmd = r"C:\Users\User\Desktop\xi410_direct_sdk_project\xi410_tcp_trigger\build\bin\Debug\xi410_tcp_trigger.exe  -e C:\Users\User\Desktop\xi410_direct_sdk_project\tools\test -n example.csv -p"
 
+
 def run_cmd(n, delay):
+    global successCnt
+    successCnt = 0
     for i in range(n):
         print(f"Running CMD instance {i+1}/{n}...")
-        subprocess.run(cmd, shell=True)  # Execute cmd.exe
+        result = subprocess.run(cmd, shell=True)  # Execute cmd.exe
+         # Check return code
+        if result.returncode != 0:
+            print(f"❌ Error: Command failed with exit code {result.returncode}")
+            print(f"🔹 STDOUT: {result.stdout.strip()}")
+            print(f"🔺 STDERR: {result.stderr.strip()}") 
+        else:
+            successCnt = successCnt + 1
         if i < n - 1:  # Avoid sleeping after the last execution
             time.sleep(delay)
 
@@ -19,3 +29,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     run_cmd(args.n, args.delay)
+
+    print(f"{successCnt}/{args.n} executed successfully!")
